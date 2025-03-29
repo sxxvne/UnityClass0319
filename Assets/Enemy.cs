4f9fbd29c056ecfd7dd53f11496b28f7695a399e
sxxvne <42821067a@gmail.com>
2025-03-29 13:32:59 +0900

    동아리과제수정

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -7,22 +7,45 @@ public class Enemy : MonoBehaviour
 {
     public Transform player;
     private float detectionRange = 20f;
+    public float attackRange = 2f;     
+    public int damage = 10;
 
     private NavMeshAgent agent;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player"); // 플레이어 찾기
+
+        if (playerObject != null)
+        {
+            player = playerObject.transform;
+
+        }
     }
 
     private void Update()
     {
+        if (agent != null) return;
         float distancePlayer =
             Vector3.Distance(player.position, transform.position);
-        if (distancePlayer <  detectionRange)
+        if (distancePlayer < detectionRange)
         {
-            agent.SetDestination(player.position);
+            agent.SetDestination(player.transform.position);
         }
 
+
+
+        if (distancePlayer <= attackRange)
+        {
+           Hp playerHp = player.GetComponent<Hp>();
+           if (playerHp != null) 
+           { playerHp.Damage(damage);
+           }
+        }
     }
 }
+   
+
+
